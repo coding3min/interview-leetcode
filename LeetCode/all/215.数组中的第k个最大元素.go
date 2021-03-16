9fb8bf37f0e3ea73e311ab800e5e7b7b1d0e6261
minibear2333 <pzquu@qq.com>
2021-03-16 20:04:32 +0800

    [*]网络协议相关问题

--- LeetCode/all/215.数组中的第k个最大元素.go
+++ LeetCode/all/215.数组中的第k个最大元素.go
@@ -33,6 +33,42 @@
  */
 
 // @lc code=start
+func findKthLargest(nums []int, k int) int {
+    rand.Seed(time.Now().UnixNano())
+    return quickSelect(nums, 0, len(nums)-1, len(nums)-k)
+}
+
+// 快排剪枝
+func quickSelect(a []int, l, r, index int) int {
+    q := randomPartition(a, l, r)
+    if q == index {
+        return a[q]
+    } else if q < index {
+        return quickSelect(a, q + 1, r, index)
+    }
+    return quickSelect(a, l, q - 1, index)
+}
+
+//随机中枢
+func randomPartition(a []int, l, r int) int {
+    i := rand.Int() % (r - l + 1) + l
+    a[i], a[r] = a[r], a[i]
+    return partition(a, l, r)
+}
+
+//快排核心代码
+func partition(a []int, l, r int) int {
+    x := a[r]
+    i := l - 1
+    for j := l; j < r; j++ {
+        if a[j] <= x {
+            i++
+            a[i], a[j] = a[j], a[i]
+        }
+    }
+    a[i+1], a[r] = a[r], a[i+1]
+    return i + 1
+}
 // 肯定不能用这种无赖的办法
 // func findKthLargest(nums []int, k int) int {
 // 	sort.Ints(nums)
