a3b0eac164b8a4226e99a5c20dde98f9dfe37234
minibear2333 <pzquu@qq.com>
2021-07-22 22:02:46 +0800

    feat(32): 最长有效括号

---
+++ LeetCode/all/LeetCode/all/32.最长有效括号.go
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=32 lang=golang
+ *
+ * [32] 最长有效括号
+ *
+ * https://leetcode-cn.com/problems/longest-valid-parentheses/description/
+ *
+ * algorithms
+ * Hard (35.20%)
+ * Likes:    1378
+ * Dislikes: 0
+ * Total Accepted:    168.1K
+ * Total Submissions: 476.1K
+ * Testcase Example:  '"(()"'
+ *
+ * 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：s = "(()"
+ * 输出：2
+ * 解释：最长有效括号子串是 "()"
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：s = ")()())"
+ * 输出：4
+ * 解释：最长有效括号子串是 "()()"
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：s = ""
+ * 输出：0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * s[i] 为 '(' 或 ')'
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+func longestValidParentheses(s string) int {
+    maxAns := 0
+    dp := make([]int, len(s))
+    for i := 1; i < len(s); i++ {
+        if s[i] == ')' {
+            if s[i-1] == '(' {
+                if i >= 2 {
+                    dp[i] = dp[i - 2] + 2
+                } else {
+                    dp[i] = 2
+                }
+            } else if i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '(' {
+                if i - dp[i - 1] >= 2 {
+                    dp[i] = dp[i - 1] + dp[i - dp[i - 1] - 2] + 2
+                } else {
+                    dp[i] = dp[i - 1] + 2
+                }
+            }
+            maxAns = max(maxAns, dp[i])
+        }
+    }
+    return maxAns
+}
+
+func max(x,y int) int{
+	if x>y{
+		return x
+	}
+	return y
+}
+// @lc code=end
+
