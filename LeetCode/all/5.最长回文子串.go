b9bd645477fd97c29bd53821365a671437c5aa94
Obsession-kai <68723187+Obsession-kai@users.noreply.github.com>
2021-08-02 22:16:26 +0800

    Update 5.最长回文子串.go

--- LeetCode/all/5.最长回文子串.go
+++ LeetCode/all/5.最长回文子串.go
@@ -59,7 +59,52 @@
 
 // @lc code=start
 func longestPalindrome(s string) string {
+	n := len(s)
+	if n <= 1{
+		return s
+	}
+	// begin 和 max_length 为当前最长回文子串的开始位置和长度
+	max_len := 1
+	begin := 0
+	dp := make([][]bool,n)
+	// 长度为 1 的字符串均为回文串
+	for i := 0;i < n;i++{
+		dp[i] = make([]bool,n)
+		dp[i][i] = true
+	}
 
+	// 枚举子串长度，从 2 开始，最长为 n
+	for L := 2;L < n + 1;L++{
+		// 枚举左边界
+		for left := 0;left < n;left++{
+			// 固定字符串长度下的右边界
+			right := L + left - 1
+			// 右边界越界，则退出此长度下的循环
+			if right >= n{
+				break
+			}
+			// 如果左右边界字符不同，则一定不是回文串
+			if s[left] != s[right]{
+				dp[left][right] = false
+			} else {
+				// right - left <= 2 等价于 字符串长度 <= 3
+				// 此时只需要字符串左右两端字符相等，即为回文串
+				if right - left <= 2{
+					dp[left][right] = true
+				} else {
+					// 该情况下，字符串左右边界符合回文串条件，
+					// 改字符串是否为回文串要根据左右各缩进一位的字符串是否为回文串来进行判断
+					dp[left][right] = dp[left + 1][right - 1]
+				}
+			}
+			// 更新最长回文子串
+			if dp[left][right] && right - left + 1 > max_len{
+				max_len = right - left + 1
+				begin = left
+			}
+		}
+	}
+	return s[begin:begin + max_len]
 }
 // @lc code=end
 
